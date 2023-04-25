0aae7260ce3bf8b7dedfe0cf888757ef5ce4cc67
JohanFernandoLema <johan.13fernando@gmail.com>
2023-04-25 16:24:34 -0400

    array of objects that contain our menu created

---
+++ app.js
@@ -0,0 +1,75 @@
+const menu = 
+[
+    {
+        id: 1,
+        title: "encebollado",
+        category: "lunch",
+        price:14.99,
+        img: "./Assets/images/./Assets/images/encebollado.png",
+        desc: "soup with shredded fish, sliced onions, yuca, pop corn and plantain chips "
+    },
+    {
+        id: 2,
+        title: "coconut milkshake",
+        category: "shakes",
+        price: 6.99,
+        img: "./Assets/images/coconut.jpg",
+        desc: "coconut blended with milk and chocolate, additional one scoope of our homemade ice-cream"
+    },
+    {
+        id: 3,
+        title: "bolon and coffe",
+        category: "breakfast",
+        price: 9.99,
+        img: "./Assets/images/bolon.jpg",
+        desc: "cooked and smashed plantain with pork, cheese or chicharron inside of it. It will be molded like a sphere and then it will be deep-fried.Additional to it a cup of coffe"
+    },
+    {
+        id: 4,
+        title:"tigrillo",
+        category: "breakfast",
+        price: 12.99,
+        img: "./Assets/images/tigrillo.jpg",
+        desc: "Smashed plantain with scrambled eggs, pork and cheese."
+    },
+    {
+        id: 5,
+        title: "oreo dream",
+        category: "shakes",
+        price: 18.99,
+        img: "./Assets/images/oreo.jpg",
+        desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+    },
+    {
+        id: 6,
+        title: "bacon overflow",
+        category: "shake",
+        price: 8.99,
+        img: "./Assets/images/fanesca.jpeg",
+        desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+    },
+    {
+        id: 7,
+        title: "bacon overflow",
+        category: "breakfast",
+        price: 8.99,
+        img: "./Assets/images/fanesca.jpeg",
+        desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+    },
+    {
+        id: 8,
+        title: "fanesca",
+        category: "lunch",
+        price: 12.99,
+        img: "./Assets/images/fanesca.jpeg",
+        desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,     
+    },
+    {
+        id: 9,
+        title: "quarantine buddy",
+        category: "shakes",
+        price: 16.99,
+        img: "./Assets/images/fanesca.jpeg",
+        desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+    },
+]
